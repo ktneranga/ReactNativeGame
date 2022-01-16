357cae89d570220751801420646046d67bb14212
ktneranga <teran.neranga@gmail.com>
2022-01-17 01:11:51 +0530

    images

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -7,7 +7,9 @@ const GameOverScreen = (props) => {
     return(
         <View style={styles.screen}>
             <TitleText>The Game is Over!</TitleText>
-            <Image source={require('../assets/images/success.png')} />
+            <View style={styles.imageContainer}>
+                <Image source={require('../assets/images/success.png')} style={styles.image} />
+            </View>
             <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
             <BodyText>Number was: {props.userNumber}</BodyText>
             <Button title='New Game' onPress={props.onRestart}/>
@@ -21,6 +23,19 @@ const styles = StyleSheet.create({
         padding: 10,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    imageContainer: {
+        width: '80%',
+        height: 300,
+        borderColor: 'black',
+        borderWidth: 2,
+        borderRadius: 200,
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    image : {
+        width: '100%',
+        height: '100%'
     }
 });
 
